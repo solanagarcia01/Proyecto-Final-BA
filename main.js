d3b93807e3a487b0fd785e354916a69232a23a3f
Solana Garcia <solanagarcia01@gmail.com>
2023-11-13 10:59:12 -0300

    Ultimas modificaciones

--- main.js
+++ main.js
@@ -1,6 +1,42 @@
-let variable = 0;
-let variableDos = 3;
+let currentIndex = 0;
+const carousel = document.querySelector('.carousel');
 
-let variable1 = "Hola";
+function nextImage() {
+    currentIndex++;
+    if (currentIndex === carousel.children.length) {
+        currentIndex = 0;
+    }
+    updateCarousel();
+}
 
-let resultado = varible + variableDos;
+function updateCarousel() {
+    const offset = -currentIndex * 300; // Ancho de cada imagen
+    carousel.style.transform = `translateX(${offset}px)`;
+}
+
+setInterval(nextImage, 2000); // Cambia de imagen cada 2 segundos
+
+
+let edad = prompt("Ingrese su edad:")
+
+if (edad >= 18) {
+    alert("Danos tu sugerencia!")
+}
+else {
+    alert("No puedes completar esta secci�n")
+}
+
+let elemento = document.querySelector("h2");
+elemento.addEventListener("click",function click(){
+    alert ("Conocé Buenos Aires")
+});
+
+function mostrar(){
+    document.getElementById("corazon").style.display="block";
+
+}
+
+function ocultar(){
+    document.getElementById("corazon").style.display="none";
+}
+
